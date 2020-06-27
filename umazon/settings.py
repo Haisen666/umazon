a394c7fecb1db5ce82feb1bac65d346efeed6016
Haisen666 <liupeixuan516@yahoo.co.jp>
2020-06-27 10:29:15 +0900

    0627

--- umazon/settings.py
+++ umazon/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from django.conf import settings
+from django.conf.urls.static import static
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,10 +126,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+
+STATICFILES_DIRS =[
+     os.path.join(BASE_DIR, "static")
+]
+
+
+
 
 LOGIN_URL = '/login'
 LOGIN_REDIRECT_URL = '/user_index'
