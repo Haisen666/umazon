5e319b6ff264d1e00ecf7715366636ed6834e6bb
Haisen666 <liupeixuan516@yahoo.co.jp>
2020-07-15 21:36:17 +0900

    0715_bug

---
+++ buy/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from buy.models import Buy
+
+class BuyForm(forms.ModelForm):
+    class Meta:
+        model = Buy
+        fields = (
+            'product_num',
+            'name',
+            'price',
+            'count',
+            'total_price'
+        )
--- buy/views.py
+++ buy/views.py
@@ -7,29 +7,32 @@ from django.template.response import TemplateResponse
 from cart.models import Cart
 from buy.models import Buy
 from product.models import Product
-
+from buy.forms import BuyForm
 # Create your views here.
 def buy_list(request):
 
     cart = Cart.objects.filter(user=request.user.id)
-    # buy = Buy.
+
     if request.method =='POST':
         count=0
         for c in cart:
-            # product=Product.objects.get(id=c.product_num)
-            # c.name=product.name
-            # c.product_num = product.id
-            buy = Buy()
-            buy.user = request.user.id
-            buy.product_num = c.product_num
-            buy.price = c.price
-            buy.count = c.count
-            buy.total_price = c.price*c.count
-            buy.save()
-            count+=1
-
+            form = BuyForm(request.POST)
+            if form.is_valid():
+                buy = Buy()
+                buy.user = request.user.id
+                buy.product_num = c.product_num
+                buy.price = c.price
+                buy.count = c.count
+                buy.total_price = c.price*c.count
+                buy.save()
+                count+=1
+        else:
+            form = BuyForm(None)
+    # buy = Buy.
+    
+    buy=Buy.objects.all()
     if buy:
-        # return HttpResponseRedirect(reverse('buy_list'))
+        # return HttpResponseRedirect(reverse('buy_list'),{'buy':buy,'count':count})
         return TemplateResponse(request,'buy/buy_list.html',{'buy':buy,'count':count})
     else:
         return TemplateResponse(request,'top/toppage.html')
